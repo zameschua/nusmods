c595e52923a0708e06d54f9b0f3ba475630f8f0a
Li Kai <falconets25601@gmail.com>
2017-12-28 17:59:34 +0800

    Use common eslint configuration

--- packages/nusmoderator/.eslintrc.js
+++ packages/nusmoderator/.eslintrc.js
@@ -1,38 +1,12 @@
 module.exports = {
-  parser: 'babel-eslint',
-  root: true,
-  extends: [
-    'airbnb-base',
-    'plugin:flowtype/recommended',
-  ],
   env: {
-    browser: true,
-    node: true,
+    'shared-node-browser': true,
   },
-  globals: {
-    expect: true,
-    it: true,
-  },
-  plugins: [
-    'flowtype',
-    'import',
-  ],
   settings: {
     'import/resolver': {
       webpack: {
         config: 'webpack.config.js',
       },
     },
-  },
-  rules: {
-    'arrow-body-style': 'off',
-    'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
-    'max-len': ['error', 120],
-    'import/extensions': ['error', 'always',
-      {
-        js: 'never',
-      }
-    ],
-    'linebreak-style': 'off',
   },
 };
